b9ac45d1f03a1e2b8b2722bcfe52d9c6a5711776
RVBelyaevsky <rooooooma73@gmail.com>
2024-08-29 12:50:56 +0800

    24.2 (задание 3)

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,19 +3,20 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from lms.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = ('lesson_title',)
+
+
 class CourseSerializer(ModelSerializer):
     count_lessons = SerializerMethodField()
+    lessons = LessonSerializer(many=True, source='lesson_set.all')
 
     def get_count_lessons(self, obj):
         return obj.lesson_set.count()
 
     class Meta:
         model = Course
-        fields = ('course_title', 'course_description', 'count_lessons')
-
-
-class LessonSerializer(ModelSerializer):
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+        fields = ('course_title', 'course_description', 'count_lessons', 'lessons')
